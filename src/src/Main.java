925c13a863da0c9847ae1f6d4f732379d425ec24
adidamusahithi <adidamusahithi@gmail.com>
2017-09-12 14:30:04 +0530

    uploading ..

---
+++ src/src/Main.java
@@ -0,0 +1,22 @@
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		String sDate1="1998/06/15"; 
+		//Date date1=new SimpleDateFormat("yyyy/MM/dd").parse(sDate1);  
+		//System.out.println(" "+date1+" ");
+		//Student stgp = new Student(1,"Adidamu Sahithi",date1,330.3);
+		//StudentGroup st = new StudentGroup(5);
+		Student stgp = new Student(1,"Adidamu Sahithi","1997/09/11",330.3);
+
+
+
+		
+	}
+
+}
